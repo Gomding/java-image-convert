6d00d54e568f7e21e1b05e33e00b73081a3a95e1
gomding <backchida@gmail.com>
2021-09-21 18:03:58 +0900

    refactor: 불필요한 코드 제거

--- src/test/java/com/example/javaimageconvert/ImageTest.java
+++ src/test/java/com/example/javaimageconvert/ImageTest.java
@@ -7,13 +7,10 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 class ImageTest {
 
-    private static final String GIF_FILENAME = "static/image/jjv1FK.gif";
-    private static final String MP4_FILENAME = "static/image/conver_jjv1FK.mp4";
     private static final String PNG_FILENAME = "static/image/mickey.png";
     private static final String JPG_FILENAME = "static/image/mickey2222.png";
 
@@ -35,7 +32,7 @@ class ImageTest {
         Graphics graphics = bufferedImage.getGraphics();
 
 //        Color invisible = new Color(0,0,0,);
-        graphics.drawImage(resizedImage, 0, 0,null);
+        graphics.drawImage(resizedImage, 0, 0, null);
         ImageIO.write(bufferedImage, "png", newFile);
 
         graphics.dispose();
