5dcfe252027d71552d0c698f7388ceb400541d91
gomding <backchida@gmail.com>
2021-09-29 19:40:07 +0900

    refactor(JavaCvFFmpegTest): JavaCv와 FFmpeg-cli-wrapper 를 함께 사용하도록 구현

--- src/test/java/com/example/javaimageconvert/JavaCvFFmpegTest.java
+++ src/test/java/com/example/javaimageconvert/JavaCvFFmpegTest.java
@@ -2,48 +2,28 @@ package com.example.javaimageconvert;
 
 import net.bramp.ffmpeg.FFmpeg;
 import net.bramp.ffmpeg.FFmpegExecutor;
-import net.bramp.ffmpeg.FFprobe;
 import net.bramp.ffmpeg.builder.FFmpegBuilder;
 import net.bramp.ffmpeg.job.FFmpegJob;
+import org.bytedeco.javacpp.Loader;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest
-class FfmpegTest {
-
-    @Value("${ffmpeg.path}")
-    private String ffmpegPath;
-
-    @Value("${ffprobe.path}")
-    private String ffprovePath;
+class JavaCvFFmpegTest {
 
     private static final String GIF_FILENAME = "static/image/jjv1FK.gif";
 
+    String ffmpegPath = Loader.load(org.bytedeco.ffmpeg.ffmpeg.class);
+
     @Test
     void test() {
-        System.out.println(ffmpegPath);
-        System.out.println(ffprovePath);
-
         URL resource = getClass().getClassLoader().getResource(GIF_FILENAME);
         File gifFile = new File(resource.getPath());
-
         File mp4File = new File(gifFile.getPath().replace(".gif", ".mp4"));
 
         try {
-            final FFmpeg fFmpeg = new FFmpeg(ffmpegPath);
-            final FFprobe fFprobe = new FFprobe(ffprovePath);
-            System.out.println("initComplete!!!");
-
-            assertThat(fFmpeg.isFFmpeg()).isTrue();
-            assertThat(fFprobe.isFFprobe()).isTrue();
-
             FFmpegBuilder builder = new FFmpegBuilder()
                     .overrideOutputFiles(true)
                     .addExtraArgs("-r", "10")
@@ -57,17 +37,10 @@ class FfmpegTest {
                     .setVideoFilter("scale=trunc(iw/2)*2:trunc(ih/2)*2")
                     .done();
 
-            System.out.println(builder.build());
-            builder.build().forEach(System.out::println);
-
-            FFmpegExecutor executor = new FFmpegExecutor(fFmpeg, fFprobe);
+            FFmpegExecutor executor = new FFmpegExecutor(new FFmpeg(ffmpegPath));
             FFmpegJob job = executor.createJob(builder);
-            System.out.println("before run job state!!! is " + job.getState());
             job.run();
-            System.out.println("after run job state!!! is " + job.getState());
-
         } catch (IOException e) {
-            System.out.println("fail!!!");
             e.printStackTrace();
         }
     }
